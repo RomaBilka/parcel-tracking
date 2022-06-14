57fed92f491108a65d766a9e2c55f4331578fb26
RomaBilka <roman_biliak@i.ua>
2022-06-14 14:09:38 +0300

    fix

--- pkg/determine-delivery/carriers/me/api_test.go
+++ pkg/determine-delivery/carriers/me/api_test.go
@@ -12,7 +12,8 @@ import (
 func TestTrackingDocument(t *testing.T) {
 	me := NewMeestExpress("0xA79E003048D2B47311E26B7D4A430FFC", "public", "PUBLIC", "https://apii.meest-group.com/T/1C_Query.php")
 
-	me.ShipmentsTrack("TESTIK11")
+	_, err := me.ShipmentsTrack("TESTIK11")
+	assert.NoError(t, err)
 }
 
 func TestFixturesTrackingDocument(t *testing.T) {
--- pkg/determine-delivery/detector.go
+++ pkg/determine-delivery/detector.go
@@ -21,10 +21,10 @@ type D interface {
 func Detect(str string) string {
 	carriers := make(map[string]D)
 
-	carriers[NOVA_POSHTA] = &np.NP{str}
-	carriers[NP_SHOPPING] = &np_shopping.NPShopping{str}
-	carriers[UPS] = &ups.UPS{str}
-	carriers[MEEST_EXPRESS] = &me.ME{str}
+	carriers[NOVA_POSHTA] = &np.NP{TrackId: str}
+	carriers[NP_SHOPPING] = &np_shopping.NPShopping{TrackId: str}
+	carriers[UPS] = &ups.UPS{TrackId: str}
+	carriers[MEEST_EXPRESS] = &me.ME{TrackId: str}
 
 	for k, carrier := range carriers {
 		if carrier.Detect() {
