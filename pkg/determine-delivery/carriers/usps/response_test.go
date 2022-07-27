4506c385d196d781e6f1a553358c8899afaa423f
Liubomyr Mykhalchyshyn <LMykhalchyshyn@slb.com>
2022-07-27 21:13:52 +0300

    usps test fixtures

--- pkg/determine-delivery/carriers/usps/response_test.go
+++ pkg/determine-delivery/carriers/usps/response_test.go
@@ -1,1 +1,34 @@
 package usps
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_response_isError(t *testing.T) {
+	type fields struct {
+		number  string
+		details []string
+	}
+	type args struct {
+		xmlBody []byte
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr assert.ErrorAssertionFunc
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := &response{
+				number:  tt.fields.number,
+				details: tt.fields.details,
+			}
+			tt.wantErr(t, r.isError(tt.args.xmlBody), fmt.Sprintf("isError(%v)", tt.args.xmlBody))
+		})
+	}
+}
