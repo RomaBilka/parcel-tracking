19dcb93cf8d7e6609b44a682b10c89b06585c48e
RomaBilka <roman_biliak@i.ua>
2022-08-01 17:41:00 +0300

    uppercase

--- dependencies/config.go
+++ dependencies/config.go
@@ -7,13 +7,13 @@ import (
 type (
 	NovaPoshta struct {
 		ApiURL string `long:"NP_API_URL" description:"nova poshta API URL" required:"true" default:"https://api.novaposhta.ua" env:"NP_API_URL"`
-		ApiKey string `long:"NP_API_Key" description:"nova poshta API key" env:"NP_API_KEY"`
+		ApiKey string `long:"NP_API_KEY" description:"nova poshta API key" env:"NP_API_KEY"`
 	}
 
 	MeestExpress struct {
 		ID       string `long:"ME_ID" description:"meest express ID" required:"true" default:"0xA79E003048D2B47311E26B7D4A430FFC" env:"ME_ID"`
-		Login    string `long:"ME_Login" description:"meest express login" required:"true" default:"public" env:"ME_LOGIN"`
-		Password string `long:"ME_Password" description:"meest express password" required:"true" default:"PUBLIC" env:"ME_PASSWORD"`
+		Login    string `long:"ME_LOGIN" description:"meest express login" required:"true" default:"public" env:"ME_LOGIN"`
+		Password string `long:"ME_PASSWORD" description:"meest express password" required:"true" default:"PUBLIC" env:"ME_PASSWORD"`
 		ApiURL   string `long:"ME_API_URL" description:"meest express API URL" required:"true" default:"https://apii.meest-group.com/T/1C_Query.php" env:"ME_API_URL"`
 	}
 
