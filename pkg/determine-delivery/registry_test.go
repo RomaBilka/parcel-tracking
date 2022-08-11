daa3685a3a998ee86c46856b695cb4fd89c97eb7
yshnitsar <yshnitsar@slb.com>
2022-08-11 15:53:39 +0300

    update registry test

--- pkg/determine-delivery/registry_test.go
+++ pkg/determine-delivery/registry_test.go
@@ -1,82 +1,59 @@
 package determine_delivery
 
 import (
-	"errors"
 	"testing"
 
 	"github.com/RomaBilka/parcel-tracking/pkg/determine-delivery/carriers"
-	"github.com/RomaBilka/parcel-tracking/pkg/determine-delivery/carriers/dhl"
-	"github.com/RomaBilka/parcel-tracking/pkg/determine-delivery/carriers/fedex"
-	"github.com/RomaBilka/parcel-tracking/pkg/determine-delivery/carriers/me"
-	"github.com/RomaBilka/parcel-tracking/pkg/determine-delivery/carriers/np"
-	np_shopping "github.com/RomaBilka/parcel-tracking/pkg/determine-delivery/carriers/np-shopping"
-	"github.com/RomaBilka/parcel-tracking/pkg/determine-delivery/carriers/ups"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
 
-func TestDetermine(t *testing.T) {
+func TestDetector_Detect_Fails(t *testing.T) {
+	trackId := "test-track-id"
+
+	m := &mockCarrier{}
+	m.On("Detect", trackId).Twice().Return(false)
+
 	detector := NewDetector()
-	npCarrier := np.NewCarrier(np.NewApi("", ""))
-	detector.Registry(npCarrier)
-	meCarrier := me.NewCarrier(me.NewApi("", "", "", ""))
-	detector.Registry(meCarrier)
-	fedexCarrier := fedex.NewCarrier(fedex.NewApi("", "", "", ""))
-	detector.Registry(fedexCarrier)
-	dhlCarrier := dhl.NewCarrier(dhl.NewApi("", ""))
-	detector.Registry(dhlCarrier)
-	upsCarrier := ups.NewCarrier(ups.NewApi("", "", "", ""))
-	detector.Registry(upsCarrier)
-	npShoppingCarrier := np_shopping.NewCarrier(np_shopping.NewApi(""))
-	detector.Registry(npShoppingCarrier)
+	detector.Registry(m)
+	detector.Registry(m)
 
-	testCases := []struct {
-		name    string
-		trackId string
-		carrier carriers.Carrier
-		err     error
-	}{
-		{name: "UPS 1Z12345E6605272234", trackId: "1Z12345E6605272234", carrier: upsCarrier},
-		{name: "UPS 1Z123456E6605272234", trackId: "1Z123456E6605272234", carrier: upsCarrier},
-		{name: "UPS 1Z123456E660527223", trackId: "1Z123456E660527223", carrier: upsCarrier},
-		{name: "UPS 1ZWX0692YP40636269", trackId: "1ZWX0692YP40636269", carrier: upsCarrier},
-		{name: "UPS cgish000116630", trackId: "cgish000116630", carrier: upsCarrier},
-		{name: "NovaPoshta 59000000000001", trackId: "59000000000001", carrier: npCarrier},
-		{name: "NovaPoshta 10000000000001", trackId: "10000000000001", carrier: npCarrier},
-		{name: "NovaPoshta 20000000000001", trackId: "20000000000001", carrier: npCarrier},
-		{name: "MeestExpress CV999999999ZZ", trackId: "CV999999999ZZ", carrier: meCarrier},
-		{name: "MeestExpress MYCV999999999ZZ", trackId: "MYCV999999999ZZ", carrier: meCarrier},
+	carrier, err := detector.Detect(trackId)
+	assert.Nil(t, carrier, "carrier should be nil")
+	assert.Equal(t, errCarrierNotDetected, err)
+}
 
-		{name: "NP Shopping NPI99999999999999", trackId: "NPI99999999999999", carrier: npShoppingCarrier},
+func TestDetector_Detect_Success(t *testing.T) {
+	trackId := "test-track-id"
 
-		{name: "DHL 0001111111111", trackId: "0001111111111", carrier: dhlCarrier},
-		{name: "DHL JVGL1111111111", trackId: "JVGL1111111111", carrier: dhlCarrier},
-		{name: "DHL GM1111111111", trackId: "GM1111111111", carrier: dhlCarrier},
-		{name: "DHL LX1111111111", trackId: "LX1111111111", carrier: dhlCarrier},
-		{name: "DHL RX1111111111", trackId: "RX1111111111", carrier: dhlCarrier},
-		{name: "DHL 3S1111111111", trackId: "3S1111111111", carrier: dhlCarrier},
-		{name: "DHL JJD1111111111", trackId: "JJD1111111111", carrier: dhlCarrier},
-		{name: "DHL 1234-12345", trackId: "1234-12345", carrier: dhlCarrier},
-		{name: "DHL 123-12345678", trackId: "1234-12345", carrier: dhlCarrier},
-		{name: "DHL ABC123456", trackId: "ABC123456", carrier: dhlCarrier},
-		{name: "DHL 1AB123456", trackId: "1AB123456", carrier: dhlCarrier},
-		{name: "DHL 1AB12345", trackId: "1AB12345", carrier: dhlCarrier},
-		{name: "DHL 1AB1234", trackId: "1AB1234", carrier: dhlCarrier},
-		{name: "DHL ABC-ABC-1234567", trackId: "ABC-ABC-1234567", carrier: dhlCarrier},
-		{name: "DHL AB-AB-1234567", trackId: "AB-AB-1234567", carrier: dhlCarrier},
-		{name: "DHL ABC-AB-1234567", trackId: "ABC-AB-1234567", carrier: dhlCarrier},
-		{name: "DHL AB-ABC-1234567", trackId: "AB-ABC-1234567", carrier: dhlCarrier},
-		{name: "Fedex 123456789012", trackId: "123456789012", carrier: fedexCarrier},
-		{name: "Fedex 123456789012345", trackId: "123456789012345", carrier: fedexCarrier},
-		{name: "Fedex 12345678901234567890", trackId: "12345678901234567890", carrier: fedexCarrier},
-		{name: "unknown", trackId: "59000", err: errors.New("carrier not detected")},
+	invalidCarrier := &mockCarrier{}
+	invalidCarrier.On("Detect", trackId).Twice().Return(false)
+
+	validCarrier := &mockCarrier{}
+	validCarrier.On("Detect", trackId).Once().Return(true)
+
+	detector := NewDetector()
+	detector.Registry(invalidCarrier)
+	detector.Registry(invalidCarrier)
+	detector.Registry(validCarrier)
+
+	carrier, err := detector.Detect(trackId)
+	assert.NoError(t, err)
+	assert.Equal(t, carrier, validCarrier)
+}
+
+type mockCarrier struct {
+	mock.Mock
+}
+
+func (m *mockCarrier) Track(s string) ([]carriers.Parcel, error) {
+	args := m.Called(s)
+	if args.Get(0) != nil {
+		return args.Get(0).([]carriers.Parcel), args.Error(1)
 	}
+	return nil, args.Error(1)
+}
 
-	for i := range testCases {
-		testCase := testCases[i]
-		t.Run(testCase.name, func(t *testing.T) {
-			carrier, err := detector.Detect(testCase.trackId)
-			assert.Equal(t, testCase.carrier, carrier)
-			assert.Equal(t, testCase.err, err)
-		})
-	}
+func (m *mockCarrier) Detect(s string) bool {
+	return m.Called(s).Bool(0)
 }
