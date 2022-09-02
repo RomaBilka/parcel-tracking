5591fe95f0f771360620189b6dfb4c8239fdfe62
RomaBilka <roman_biliak@i.ua>
2022-09-02 11:48:31 +0300

    merg

--- pkg/response-errors/response-errors.go
+++ pkg/response-errors/response-errors.go
@@ -4,7 +4,4 @@ import "errors"
 
 var InvalidNumber = errors.New("invalid number")
 var NotFound = errors.New("not found")
-<<<<<<< HEAD
 var CarrierNotFound = errors.New("carrier is not detected")
-=======
->>>>>>> b93e96ef33dc9c1b600b146c96e3941d9739c3b3
