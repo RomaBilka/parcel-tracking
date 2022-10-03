94edda3b0e2e1096adc281d41d962197fec0b4db
RomaBilka <roman_biliak@i.ua>
2022-10-03 13:21:53 +0300

    changed np-shopping logic

--- pkg/determine-delivery/carriers/np-shopping/carrier.go
+++ pkg/determine-delivery/carriers/np-shopping/carrier.go
@@ -2,6 +2,9 @@ package np_shopping
 
 import (
 	"regexp"
+	"strconv"
+	"sync"
+	"time"
 
 	"github.com/RomaBilka/parcel-tracking/pkg/determine-delivery/carriers"
 )
@@ -36,34 +39,55 @@ func (c *Carrier) Detect(trackId string) bool {
 	return false
 }
 
-func (c *Carrier) Track(trackingIds []string) ([]carriers.Parcel, error) {
-	/*response, err := c.api.TrackByTrackingNumber(trackingId)
-	if err != nil {
+func (c *Carrier) Track(trackNumbers []string) ([]carriers.Parcel, error) {
+	var mu sync.Mutex
+	var wg sync.WaitGroup
+	chanErr := make(chan error)
+	defer close(chanErr)
+	var parcels []carriers.Parcel
+
+	go func() {
+		for _, trackNumber := range trackNumbers {
+			wg.Add(1)
+			go func(trackNumber string) {
+				response, err := c.api.TrackByTrackingNumber(trackNumber)
+				if err != nil {
+					chanErr <- err
+					return
+				}
+
+				places := make([]carriers.Place, len(response.TrackingHistory))
+				for i, d := range response.TrackingHistory {
+					timestamp, err := strconv.ParseInt(d.Date, 10, 64)
+					if err != nil {
+						chanErr <- err
+						return
+					}
+
+					places[i] = carriers.Place{
+						Country: d.Country,
+						Comment: d.Description,
+						Date:    time.Unix(timestamp, 0),
+					}
+				}
+
+				parcel := carriers.Parcel{
+					TrackingNumber: response.WaybillNumber,
+					Status:         response.State,
+					Places:         places,
+				}
+
+				mu.Lock()
+				parcels = append(parcels, parcel)
+				mu.Lock()
+			}(trackNumber)
+		}
+	}()
+
+	if err := <-chanErr; err != nil {
 		return nil, err
 	}
 
-	places := make([]carriers.Place, len(response.TrackingHistory))
-	for i, d := range response.TrackingHistory {
-		timestamp, err := strconv.ParseInt(d.Date, 10, 64)
-		if err != nil {
-			panic(err)
-		}
-
-		places[i] = carriers.Place{
-			Country: d.Country,
-			Comment: d.Description,
-			Date:    time.Unix(timestamp, 0),
-		}
-	}
-
-	parcels := []carriers.Parcel{
-		carriers.Parcel{
-			TrackingNumber: response.WaybillNumber,
-			Status:         response.State,
-			Places:         places,
-		},
-	}
-
-	return parcels, nil*/
-	return nil, nil
+	wg.Wait()
+	return parcels, nil
 }
